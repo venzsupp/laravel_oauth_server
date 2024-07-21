8a21e2cac06bd600e502c81502325b344443841a
venzsupp <support@venzupp.com>
2024-07-21 18:34:47 +1200

    login check

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -17,12 +17,15 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-        Auth::guard()->attempt($credentials);
-        return response()->json(['message' => 'Logged in successfully'], 201);
+        if (Auth::guard()->attempt($credentials)) {
+            return response()->json(['message' => 'Logged in successfully'], 201);
+        }
+        return response()->json(['message' => 'Logged in failed'], 500);
     }
 
     public function token()
     {
         return response()->json(['csrf_token' => csrf_token()]);
+        // 34-- 16th
     }
 }
